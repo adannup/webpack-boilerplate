616fb1befbc98c37fe8256904c64e172d7f71c6c
Ramon Adan <dann.tesla@gmail.com>
2020-06-28 19:25:46 -0500

    minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const webpackProdConfig = require('./webpack/webpack.prod');
+const webpackProd = require('./webpack/webpack.prod');
 
 const environment = (process.env.NODE_ENV || 'production').trim();
 if (environment === 'production') {
-  module.exports = webpackProdConfig;
+  module.exports = webpackProd;
 }
