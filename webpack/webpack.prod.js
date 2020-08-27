37c43dca7a65064b23dfe791327d01e1e3f02cca
Ramon Adan <dann.tesla@gmail.com>
2020-08-27 18:27:10 -0500

    fix prettier eslint (#11)

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -8,24 +8,27 @@ const PATHS = require('../PATHS');
 const { isProductionENV } = require('../utils/enviroment');
 const webpackCommon = require('./webpack.common');
 
-const getBundleAnalyzerPlugin = () => new BundleAnalyzerPlugin({
-  analyzerMode: 'static',
-  reportFilename: 'report.html',
-  openAnalyzer: false,
-});
+const getBundleAnalyzerPlugin = () =>
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    reportFilename: 'report.html',
+    openAnalyzer: false,
+  });
 
-const getMiniCssExtractPlugin = () => new MiniCssExtractPlugin({
-  filename: isProductionENV()
-    ? `${PATHS.assets.css.output}/[name].[contenthash].css`
-    : `${PATHS.assets.css.output}/[name].css`,
-  chunkFilename: isProductionENV()
-    ? `${PATHS.assets.css.output}/[id].[contenthash].css`
-    : '[id].css',
-});
+const getMiniCssExtractPlugin = () =>
+  new MiniCssExtractPlugin({
+    filename: isProductionENV()
+      ? `${PATHS.assets.css.output}/[name].[contenthash].css`
+      : `${PATHS.assets.css.output}/[name].css`,
+    chunkFilename: isProductionENV()
+      ? `${PATHS.assets.css.output}/[id].[contenthash].css`
+      : '[id].css',
+  });
 
-const getForkTsCheckerWebpackPlugin = () => new ForkTsCheckerWebpackPlugin({
-  async: false,
-});
+const getForkTsCheckerWebpackPlugin = () =>
+  new ForkTsCheckerWebpackPlugin({
+    async: false,
+  });
 
 module.exports = merge(webpackCommon, {
   mode: 'production',
