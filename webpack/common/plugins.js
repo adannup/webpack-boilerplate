05a29938c042d55c32ad8e905bdcbe58fd40e1f0
Ramon Adan <dann.tesla@gmail.com>
2020-08-26 20:05:08 -0500

    add webpack common file, and implements webpack-merge package

--- webpack/common/plugins.js
+++ webpack/common/plugins.js
@@ -1,5 +1,6 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const PATHS = require('../../PATHS');
 const { getProjectName, isProductionENV } = require('../../utils/enviroment');
 
