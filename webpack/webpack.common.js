78f830fe128211ffc5da9f679079ff3b9208815b
Ramon Adan <dann.tesla@gmail.com>
2020-08-29 19:21:37 -0500

    Improve optimization, cache and splitChunks (#16)

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -5,9 +5,8 @@ const { isProductionENV } = require('../utils/enviroment');
 module.exports = {
   context: process.cwd(), // to automatically find tsconfig.json
   output: {
-    filename: isProductionENV() ? '[name].bundle.[chunkhash].js' : '[name].bundle.[hash].js',
+    filename: isProductionENV() ? '[name].[chunkhash].js' : '[name].[hash].js',
     path: PATHS.output,
-    pathinfo: false, // https://webpack.js.org/guides/build-performance/#output-without-path-info
   },
   module: {
     rules: loaders,
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -7,14 +7,32 @@ const webpackCommon = require('./webpack.common');
 const PATHS = require('../PATHS');
 
 const getForkTsCheckerWebpackPlugin = () =>
+  // TypeScript Loader
+  // Speeds up TypeScript type checking and ESLint linting (by moving each to a separate process)
+  // https://webpack.js.org/guides/build-performance/#typescript-loader
+  // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin#readme
   new ForkTsCheckerWebpackPlugin({
+    // ESLint options
+    // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin#eslint-options
     eslint: {
-      enabled: true,
+      // files: required - same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
       files: './src/**/*.{ts,tsx,js,jsx}',
+    },
+    // IMPORTANT: If you are using fork-ts-checker-webpack-plugin alongside HappyPack or thread-loader
+    // then ensure you set the syntactic diagnostic option like so:
+    // https://github.com/TypeStrong/ts-loader#happypackmode
+    typescript: {
+      diagnosticOptions: {
+        semantic: true,
+        syntactic: true,
+      },
     },
   });
 
 const getForkTsCheckerNotifierWebpackPlugin = () =>
+  // https://github.com/johnnyreilly/fork-ts-checker-notifier-webpack-plugin#readme
+  // This is a webpack plugin that uses the node-notifier package to display build status
+  // notifications to the user. It's purpose is to work with the fork-ts-checker-webpack-plugin.
   new ForkTsCheckerNotifierWebpackPlugin({
     title: 'TypeScript',
     excludeWarnings: false,
@@ -23,10 +41,13 @@ const getForkTsCheckerNotifierWebpackPlugin = () =>
 module.exports = merge(webpackCommon, {
   mode: 'development',
   entry: {
-    app: ['react-hot-loader/patch', PATHS.entry, 'webpack-hot-middleware/client'],
+    main: ['react-hot-loader/patch', PATHS.entry, 'webpack-hot-middleware/client'],
   },
   output: {
     publicPath: PATHS.develop.publicPath,
+    // Output Without Path Info
+    // https://webpack.js.org/guides/build-performance/#output-without-path-info
+    pathinfo: false,
   },
   module: {
     rules: [
@@ -36,12 +57,16 @@ module.exports = merge(webpackCommon, {
       },
     ],
   },
-  devtool: 'cheap-eval-source-map',
+  // Devtool
+  // https://webpack.js.org/guides/build-performance/#devtool
+  devtool: 'eval-cheap-module-source-map',
   plugins: [
     getForkTsCheckerWebpackPlugin(),
     getForkTsCheckerNotifierWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
   ],
+  // Avoid Extra Optimization Steps
+  // https://webpack.js.org/guides/build-performance/#avoid-extra-optimization-steps
   optimization: {
     removeAvailableModules: false,
     removeEmptyChunks: false,
